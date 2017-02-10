e47e2c91a2dd694e1eecce2053e905a09321d23b
hemanthy <hemanth48@gmail.com>
2017-02-10 13:30:27 +0530

    constants changes

--- Constants.php
+++ Constants.php
@@ -10,23 +10,22 @@ class Constants
 {
 
     // Incorrect Email or Password!!!
-    const INCORRECT_EMAIL_OR_PASSWORD  = '1';
+    const INCORRECT_EMAIL_OR_PASSWORD  = 'INCORRECT_EMAIL_OR_PASSWORD';
 
     // CONGRATULATION REGISTRATION SUCCESSFUL
-    const REGISTRATION_SUCCESSFUL  = '2';
+    const REGISTRATION_SUCCESSFUL  = 'REGISTRATION_SUCCESSFUL';
 
     // Password and Confirm Password doesn't match
-    const PASSWORD_CONFIRM_PASSWORD_DOESNOT_MATCH = '3';
+    const PASSWORD_CONFIRM_PASSWORD_DOESNOT_MATCH = 'PASSWORD_CONFIRM_PASSWORD_DOESNOT_MATCH';
 
     // LOGIN SUCCESS,
-    const LOGIN_SUCCESS = '4';
+    const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 
     // Email ID already exists
-    const EMAIL_ALREADY_EXISTS = '5';
+    const EMAIL_ALREADY_EXISTS = 'EMAIL_ALREADY_EXISTS';
 
     function showConstant() {
         echo  self::CONSTANT . "\n";
     }
 
 }
-
